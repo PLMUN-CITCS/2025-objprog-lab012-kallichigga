e7c2aff9b2ea3ef5ef1de2fd62b1eec2cda302e6
Jhester Arwen Amboy <amboyjhesterarwen_bscs@plmun.edu.ph>
2025-02-28 01:02:43 +0800

    2025-OBJPROG-LAB012

---
+++ MethodDemo.java
@@ -0,0 +1,19 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+
+
+        public static void greet() {
+            System.out.println("Hello from the greet method!");
+            
+        }
+        greet();
+        public class MethodDemo {
+            //... (main and greet methods)...
+        
+            public static void printMessage(String message) {
+                System.out.println(message);
+            }
+        }
+        printMessage("This is my message!");
+    }
+}
